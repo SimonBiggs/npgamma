59683245fd9954511c3583285cc7e1bf3f493cf4
SimonBiggs <mail@simonbiggs.net>
2015-10-06 20:45:04 +1100

    uploaded to pip updated example

--- setup.py
+++ setup.py
@@ -2,15 +2,14 @@ from setuptools import setup
 
 setup(
     name = "npgamma",
-    version = "0.1",
+    version = "0.2",
     author = "Simon Biggs",
     author_email = "mail@simonbiggs.net",
     description = "Perform a gamma evaluation comparing two arbitrarily sized dose grids",
-    keywords = "radiotherapy, gamma evaluation, distance to agreement",
+    keywords = ["radiotherapy", "gamma evaluation", "distance to agreement", "medical physics"],
     url = "https://github.com/SimonBiggs/npgamma/",
     dowload_url = "https://github.com/SimonBiggs/npgamma/tarball/0.1",
     packages = ["npgamma"],
     license='AGPL3+',
-    long_description=open("README.md").read(),
     classifiers = [],
 )
