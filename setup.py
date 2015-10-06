8bc0f149791de8b3d2549950b633b768fff0a5de
SimonBiggs <mail@simonbiggs.net>
2015-10-07 09:29:26 +1100

    fixed setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     author = "Simon Biggs",
     author_email = "mail@simonbiggs.net",
     description = "Perform a gamma evaluation comparing two arbitrarily sized dose grids",
-    long_description = "Using numpy to find the gamma index / gamma evaluation. This gamma index is often used in Medical Physics. Example of the code in action can be found [here](http://nbviewer.ipython.org/github/SimonBiggs/npgamma/blob/master/example.ipynb). USE AT YOUR OWN RISK"
+    long_description = "Using numpy to find the gamma index / gamma evaluation. This gamma index is often used in Medical Physics. Example of the code in action can be found [here](http://nbviewer.ipython.org/github/SimonBiggs/npgamma/blob/master/example.ipynb). USE AT YOUR OWN RISK",
     keywords = ["radiotherapy", "gamma evaluation", "gamma index", "distance to agreement", "medical physics"],
     url = "https://github.com/SimonBiggs/npgamma/",
     dowload_url = "https://github.com/SimonBiggs/npgamma/tarball/0.1",
