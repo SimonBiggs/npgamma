14cdb95fae120474aefe8b1895313ee4276bfbe7
SimonBiggs <mail@simonbiggs.net>
2015-10-09 01:27:32 +1100

    headings in example 3d

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 setup(
     name = "npgamma",
-    version = "0.3.2",
+    version = "0.3.3",
     author = "Simon Biggs",
     author_email = "mail@simonbiggs.net",
     description = "Perform a gamma evaluation comparing two arbitrarily sized dose grids",
-    long_description = "Using numpy to find the gamma index / gamma evaluation. This gamma index is often used in Medical Physics. Example of the code in action can be found [here](http://nbviewer.ipython.org/github/SimonBiggs/npgamma/blob/master/example.ipynb). USE AT YOUR OWN RISK",
+    long_description = "Using numpy to find the gamma index / gamma evaluation. This gamma index is often used in Medical Physics. Example of the code in action can be found [here (3d)](http://nbviewer.ipython.org/github/SimonBiggs/npgamma/blob/master/example_3d.ipynb), and [here (2d)](http://nbviewer.ipython.org/github/SimonBiggs/npgamma/blob/master/example_2d_equal_grid.ipynb). USE AT YOUR OWN RISK",
     keywords = ["radiotherapy", "gamma evaluation", "gamma index", "distance to agreement", "medical physics"],
     url = "https://github.com/SimonBiggs/npgamma/",
     dowload_url = "https://github.com/SimonBiggs/npgamma/tarball/0.1",
