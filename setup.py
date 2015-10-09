0f8b0d0ffe8e5b999ffcf9f53bfaba6db67606cb
SimonBiggs <mail@simonbiggs.net>
2015-10-09 21:29:19 +1100

    added higher resolution example

--- setup.py
+++ setup.py
@@ -9,7 +9,6 @@ setup(
     long_description = "Using numpy to find the gamma index / gamma evaluation. This gamma index is often used in Medical Physics. Example of the code in action can be found [here (3d)](http://nbviewer.ipython.org/github/SimonBiggs/npgamma/blob/master/example_3d.ipynb), and [here (2d)](http://nbviewer.ipython.org/github/SimonBiggs/npgamma/blob/master/example_2d_equal_grid.ipynb). USE AT YOUR OWN RISK",
     keywords = ["radiotherapy", "gamma evaluation", "gamma index", "distance to agreement", "medical physics"],
     url = "https://github.com/SimonBiggs/npgamma/",
-    dowload_url = "https://github.com/SimonBiggs/npgamma/tarball/0.1",
     packages = ["npgamma"],
     license='AGPL3+',
     classifiers = [],
