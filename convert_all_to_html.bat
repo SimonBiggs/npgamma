11c5f2ce474dabba85260c728617de905977a4d5
SimonBiggs <mail@simonbiggs.net>
2016-11-29 20:30:47 +1100

    remove converter scripts

--- convert_all_to_html.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-
-python convert_all_to_html.py
-pause
--- convert_all_to_html.py
+++
@@ -1,7 +0,0 @@
-import os
-from glob import glob
-
-notebook_paths = glob('*.ipynb')
-
-for path in notebook_paths:
-    os.system('jupyter nbconvert --to html "' + path + '"')
